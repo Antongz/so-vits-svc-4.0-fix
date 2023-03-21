f7f8a85e89a5e86dec6dc0e97591cb263574cfbb
Antongz <sallyushaz@gmail.com>
2023-03-21 21:18:42 +0800

    fix: sos 手癌

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def load_model_func(ckpt_name,cluster_name,config_name):
     config_path = "configs/" + config_name
 
     with open(config_path, encoding='utf-8-sig', errors='ignore') as f:
-        printf("load config from %s" % config_path, f)
+        print("load config from %s" % config_path, f)
         config = json.load(f)
     spk_dict = config["spk"]
 
